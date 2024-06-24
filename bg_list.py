f513915c3c6e24be42724bc866b749f8d880e5e4
0xNeverC0RE <godsyndrome9@gmail.com>
2024-06-24 23:45:09 +0300

    seasons

--- bg_list.py
+++ bg_list.py
@@ -1,6 +1,12 @@
-bglist = ['https://i.imgur.com/N0eAUAq.jpeg',
+bglist = {'summer':{'day':[
+                    'https://i.imgur.com/N0eAUAq.jpeg',
                     'https://i.imgur.com/p1n1prS.jpeg',
-                    'https://i.imgur.com/09DrPYL.jpeg',
                     'https://i.imgur.com/GsH1VPN.jpeg',
                     'https://i.imgur.com/DwVtGvs.jpeg',
-                    'https://i.imgur.com/NoohwZd.jpeg']
+                    ],
+                    'night':[
+                        'https://i.imgur.com/09DrPYL.jpeg',
+                        'https://i.imgur.com/NoohwZd.jpeg'
+                        ]
+                    },'winter':{'day':['https://i.imgur.com/vsENGLM.jpeg','https://i.imgur.com/dyowAkj.jpeg','https://i.imgur.com/yiThy9s.jpeg'],
+                                'night':['https://i.imgur.com/OZtmeu8.jpeg']}}
