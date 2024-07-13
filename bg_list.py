1943bdb3f74b0f4f63f7e8ddd083514c5b48342b
Ilja Tsygankov <i.tsygankov@balticsolutions.lv>
2024-07-13 17:38:26 +0300

    New bgs

--- bg_list.py
+++ bg_list.py
@@ -3,10 +3,16 @@ bglist = {'summer':{'day':[
                     'https://i.imgur.com/p1n1prS.jpeg',
                     'https://i.imgur.com/GsH1VPN.jpeg',
                     'https://i.imgur.com/DwVtGvs.jpeg',
+                    'https://i.imgur.com/dOEO4nl.jpeg'
                     ],
                     'night':[
                         'https://i.imgur.com/09DrPYL.jpeg',
-                        'https://i.imgur.com/NoohwZd.jpeg'
+                        'https://i.imgur.com/NoohwZd.jpeg',
+                        'https://i.imgur.com/wwmwrIq.jpeg'
                         ]
-                    },'winter':{'day':['https://i.imgur.com/vsENGLM.jpeg','https://i.imgur.com/dyowAkj.jpeg','https://i.imgur.com/yiThy9s.jpeg'],
-                                'night':['https://i.imgur.com/OZtmeu8.jpeg']}}
+                    },'winter':{'day':['https://i.imgur.com/vsENGLM.jpeg',
+                                       'https://i.imgur.com/dyowAkj.jpeg',
+                                       'https://i.imgur.com/yiThy9s.jpeg'],
+                                'night':['https://i.imgur.com/OZtmeu8.jpeg',
+                                         'https://i.imgur.com/D3OIeSL.jpeg',
+                                         'https://i.imgur.com/ShosiBd.png']}}
