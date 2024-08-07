44639513154e591dea8651101cc664f2f8199805
Student5 <i.tsygankov@balticsolutions.lv>
2024-08-08 01:06:12 +0300

    new bgs

--- bg_list.py
+++ bg_list.py
@@ -5,19 +5,22 @@ bglist = {
             'https://i.imgur.com/p1n1prS.jpeg',
             'https://i.imgur.com/GsH1VPN.jpeg',
             'https://i.imgur.com/DwVtGvs.jpeg',
-            'https://i.imgur.com/dOEO4nl.jpeg'
+            'https://i.imgur.com/dOEO4nl.jpeg',
+            'https://i.imgur.com/TpzLIyT.png',
         ],
         'night': [
             'https://i.imgur.com/09DrPYL.jpeg',
             'https://i.imgur.com/NoohwZd.jpeg',
-            'https://i.imgur.com/wwmwrIq.jpeg'
+            'https://i.imgur.com/wwmwrIq.jpeg',
+            'https://i.imgur.com/ZPYkrak.png'
         ]
     },
     'winter': {
         'day': [
             'https://i.imgur.com/vsENGLM.jpeg',
             'https://i.imgur.com/dyowAkj.jpeg',
-            'https://i.imgur.com/yiThy9s.jpeg'
+            'https://i.imgur.com/yiThy9s.jpeg',
+            'https://i.imgur.com/UQIsj9e.png'
         ],
         'night': [
             'https://i.imgur.com/OZtmeu8.jpeg',
