adbef59e05df214cd530648346ab52ee4e509660
Ilja <i.tsygankov@balticsolutions.lv>
2025-05-10 18:58:23 +0300

    Adding new backgrounds

--- bg_list.py
+++ bg_list.py
@@ -4,13 +4,30 @@ bglist = {
             'day': [
                 'https://i.imgur.com/N0eAUAq.jpeg',
                 'https://i.imgur.com/p1n1prS.jpeg',
+                'https://i.imgur.com/tdOWUgL.gif',
+                'https://i.pinimg.com/originals/55/62/e0/5562e083fd17cdf49c28d1822c312422.gif',
+                'https://pa1.aminoapps.com/6746/36d6ac7d839fedae5431c1f607ffeed33563466d_hq.gif',
+                'https://64.media.tumblr.com/9be33901822ed19d003a1373783e74c2/tumblr_muaeibftpf1rjcoeno1_500.gif',
                 'https://i.imgur.com/GsH1VPN.jpeg',
+                'https://64.media.tumblr.com/f0d8e050145912e551fdd266f3448063/c3b6626fabb66233-a9/s540x810/7b5d68b54f732d2f2164a5f89e1c7f648fe99bf9.gif',
+                'https://64.media.tumblr.com/4a100470d06662b1e66cc93d522727ad/9d1b0b49d00ceea1-8a/s540x810/a9909d1477458754b5d318f5fbbcfc9b2248b04b.gif',
                 'https://i.imgur.com/DwVtGvs.jpeg',
                 'https://i.imgur.com/dOEO4nl.jpeg',
                 'https://i.imgur.com/TpzLIyT.png',
+                'https://64.media.tumblr.com/35a54aaa60d6a373e68a03037315f69b/tumblr_msm3aaXCcr1rb06tgo3_250.gifv'
+                
             ],
             'night': [
                 'https://i.imgur.com/09DrPYL.jpeg',
+                'https://i.imgur.com/64DqTIr.gif',
+                'https://i.gifer.com/PLiW.gif',
+                'https://64.media.tumblr.com/6f33735271c5a1c9c32d83c412afea2c/tumblr_ox16c0Vnhu1wpyxh6o1_540.gif',
+                'https://media.tenor.com/CDbF3yY6IkkAAAAM/silent-hill-2.gif',
+                'https://64.media.tumblr.com/4a5cf99fbfa72f6d75b07d295177fa15/tumblr_n0hmhkXE3M1tozvp8o1_500.gif',
+                'https://giffiles.alphacoders.com/253/25399.gif',
+                'https://66.media.tumblr.com/f216e92e50de81331a36e9380ba8e07f/tumblr_puz87dCtGO1vk2qmmo1_500.gif',
+                'https://64.media.tumblr.com/dcc8cc98715e11dab13d019b27d86b17/tumblr_npqy8kHebk1tkand2o1_500.gifv',
+                'https://www.picgifs.com/games-gifs/games-gifs/silent-hill-2/picgifs-silent-hill-2-390913.gif',
                 'https://i.imgur.com/NoohwZd.jpeg',
                 'https://i.imgur.com/wwmwrIq.jpeg',
                 'https://i.imgur.com/ZPYkrak.png'
@@ -18,13 +35,24 @@ bglist = {
         },
         'winter': {
             'day': [
+                'https://www.icegif.com/wp-content/uploads/2021/12/icegif-288.gif',
+                'https://i.imgur.com/nGRMDiL.gif',
+                'https://www.icegif.com/wp-content/uploads/2021/12/icegif-289.gif',
+                'https://miro.medium.com/v2/resize:fit:1200/1*Rs2Mc151BOvWlzckeHFMLg.gif',
+                'https://i.pinimg.com/originals/e1/70/9f/e1709fd6fe1e7f5160a073cd514b8b94.gif',
                 'https://i.imgur.com/vsENGLM.jpeg',
+                'https://i.pinimg.com/originals/83/43/e1/8343e15b7e6f9c07907a5ead2531a177.gif',
                 'https://i.imgur.com/dyowAkj.jpeg',
                 'https://i.imgur.com/yiThy9s.jpeg',
+                'https://www.icegif.com/wp-content/uploads/2021/12/icegif-287.gif',
                 'https://i.imgur.com/UQIsj9e.png'
             ],
             'night': [
+                'https://lparchive.org/Silent-Hill-The-Play-Novel/Update%2034/1-street1.gif',
+                'https://lparchive.org/Silent-Hill-The-Play-Novel/Update%2016/7-Untitled2.gif',
                 'https://i.imgur.com/OZtmeu8.jpeg',
+                'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEial2MlIZK31_0SI49zlwRBddgwyi4kEHcJmBugUkqiHxeWwqp9UEt9DxSuaZO4K0o_1lwin6GvPyxN3PQ-jzKdH9rDyXb2k9MwVsRW2m54wZYJTC8RjiCKvqTdU-wHBdtzqs7KxLCvb7rl/s280/Silent+Hill+Running.gif',
+                'https://i.imgur.com/TN3KTTO.gif',
                 'https://i.imgur.com/D3OIeSL.jpeg',
                 'https://i.imgur.com/ShosiBd.png'
             ]
